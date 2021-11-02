d5b11fa0f3eccc05530ca8ef06491172ab045b89
chuchosoi93 <loctt93@gmail.com>
2021-11-02 21:34:14 +0700

    Add BE + FE
    Thêm mới FE và BE

---
+++ Backend/src/main/java/com/vti/dto/animal.java
@@ -0,0 +1,12 @@
+package com.vti.dto;
+
+public class animal {
+	public static void tinhdientich () {
+		System.out.println("Tính diện tích");
+		
+	}
+	
+	public void tinhchuvi() {
+		System.out.println("Tính chu vi");
+	}
+}
